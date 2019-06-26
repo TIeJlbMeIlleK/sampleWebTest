b316fbd5ab99873c3874977d1210df923b6fddfb
mmakarov <mmakarov@iitdgroup.ru>
2019-06-26 10:38:29 +0300

    RSHBFL-1074. Аутентификация. Отправка по шаблону

--- src/test/java/ru/iitdgroup/rshbtest/WSTests.java
+++ src/test/java/ru/iitdgroup/rshbtest/WSTests.java
@@ -3,6 +3,7 @@ package ru.iitdgroup.rshbtest;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.iitdgroup.tests.apidriver.Authentication;
 import ru.iitdgroup.tests.apidriver.Client;
 import ru.iitdgroup.tests.apidriver.DBOAntiFraudWS;
 import ru.iitdgroup.tests.apidriver.Transaction;
@@ -24,9 +25,20 @@ public class WSTests extends RSHBTests {
 
     @Test(
             priority = 0,
+            description = "Пример заполнения атрибутов аутентификации и отправки их на антифрод-сервис"
+    )
+    public void callAuth() throws JAXBException, IOException {
+        Authentication authentication = new Authentication("auth/auth1.xml");
+        ws.send(authentication);
+
+        assertEquals(200, ws.getResponseCode().intValue());
+    }
+
+    @Test(
+            priority = 1,
             description = "Пример заполнения атрибутов клиента и отправки их на антифрод-сервис"
     )
-    public void callAntifraudWSClient() throws JAXBException, IOException {
+    public void callClient() throws JAXBException, IOException {
         Client client = new Client("clients/client1.xml");
         ws.send(client);
 
@@ -34,10 +46,10 @@ public class WSTests extends RSHBTests {
     }
 
     @Test(
-            priority = 1,
+            priority = 2,
             description = "Пример заполнения атрибутов транзакции и отправки их на антифрод-сервис"
     )
-    public void callAntifraudWS() throws IOException, JAXBException {
+    public void callTransaction() throws IOException, JAXBException {
         Transaction t = new Transaction("transactions/tran1.xml");
         t.withTransactionId("5000000" + new Random().nextInt(100000));
 
