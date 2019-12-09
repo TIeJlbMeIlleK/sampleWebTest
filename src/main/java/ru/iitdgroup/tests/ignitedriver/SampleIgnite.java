3edc3390cc98bd73f160e13f50ba8c94f2cbe25c
V. Dembskiy <vdembskiy@iitdgroup.ru>
2019-12-09 13:53:07 +0300

    RSHBFL-1090 changes for testing

---
+++ src/main/java/ru/iitdgroup/tests/ignitedriver/SampleIgnite.java
@@ -0,0 +1,51 @@
+package ru.iitdgroup.tests.ignitedriver;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
+
+/**
+ * Стартер игнайта для тестовых нужд.
+ */
+public class SampleIgnite {
+
+	public static Ignite runLocalignite(boolean isClient) {
+
+		final IgniteConfiguration config = createConfig();
+		config.setClientMode(isClient);
+		Ignition.start(config);
+
+		final Ignite ignite = Ignition.ignite();
+
+		return ignite;
+
+	}
+
+	public static IgniteConfiguration createConfig() {
+		final String[] addressesConf = new String[]{"127.0.0.1"};
+		final IgniteConfiguration configuration = new IgniteConfiguration();
+		final TcpDiscoverySpi discoSpi = new TcpDiscoverySpi();
+		final TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
+		final List<String> addresses = Arrays.asList(addressesConf);
+		ipFinder
+		.setAddresses(addresses)
+		.setShared(true);
+		discoSpi.setIpFinder(ipFinder);
+
+		configuration.setDiscoverySpi(discoSpi);
+
+
+		return configuration;
+	}
+
+	public static void main(String[] args) {
+		runLocalignite(false);
+	}
+
+
+}
