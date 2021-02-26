bcebc08477aaeed2cf6ad938cca94a51b179df54
Tatyana_Afanasieva <tafanasieva@iitdgroup.ru>
2021-02-26 18:15:47 +0300

    выполнены автотесты по BIQ_7739 правила EXR_01, 02, 03, 04

---
+++ src/test/java/ru/iitdgroup/tests/cases/BIQ_7739/ExR_01_AuthenticationContactChanged.java
@@ -0,0 +1,229 @@
+package ru.iitdgroup.tests.cases.BIQ_7739;
+
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+import net.bytebuddy.utility.RandomString;
+import org.testng.annotations.Test;
+import ru.iitdgroup.intellinx.dbo.transaction.TransactionDataType;
+import ru.iitdgroup.tests.apidriver.Client;
+import ru.iitdgroup.tests.apidriver.Transaction;
+import ru.iitdgroup.tests.cases.RSHBCaseTest;
+import ru.iitdgroup.tests.webdriver.administration.WorkflowAction;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class ExR_01_AuthenticationContactChanged extends RSHBCaseTest {
+    private static final String RULE_NAME = "R01_ExR_01_AuthenticationContactChanged";
+
+    private final GregorianCalendar time = new GregorianCalendar();
+    private final List<String> clientIds = new ArrayList<>();
+    private String[][] names = {{"Ирина", "Дьякова", "Витальевна"}};
+    private static final String LOGIN = new RandomString(5).nextString();
+    private static final String LOGIN_HASH = (ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE) + "").substring(0, 5);
+    //private String[] loginHashes = new String[0];
+   // private String[] logins = new String[0];
+
+
+    @Test(
+            description = "Настройка и включение правила R01_ExR_01_AuthenticationContactChanged"
+    )
+    public void enableRules() {
+        getIC().locateRules()
+                .selectVisible()
+                .deactivate()
+                .selectRule(RULE_NAME)
+                .activate()
+                .sleep(15);
+    }
+
+    @Test(
+            description = "Создаем клиента",
+            dependsOnMethods = "enableRules"
+    )
+    public void client() {
+        //Arrays.setAll(loginHashes, p -> (ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE) + "").substring(0, 5));
+        //Arrays.setAll(logins, p -> new RandomString(5).nextString());
+        try {
+            for (int i = 0; i < 1; i++) {
+                String dboId = (ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE) + "").substring(0, 9);
+                Client client = new Client("testCases/Templates/client.xml");
+
+                client.getData()
+                        .getClientData()
+                        .getClient()
+                        .withLogin(LOGIN)
+                        .withFirstName(names[i][0])
+                        .withLastName(names[i][1])
+                        .withMiddleName(names[i][2])
+                        .getClientIds()
+                        .withLoginHash(LOGIN_HASH)
+                        .withDboId(dboId)
+                        .withCifId(dboId)
+                        .withExpertSystemId(dboId)
+                        .withEksId(dboId)
+                        .getAlfaIds()
+                        .withAlfaId(dboId);
+
+                sendAndAssert(client);
+                clientIds.add(dboId);
+                System.out.println(dboId);
+            }
+        } catch (JAXBException | IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+//TODO перед запуском должен быть создан WF Action, если не создан, то запустить следующий тест enableFlags
+
+//    @Test(
+//            description = "Создаем ACTION для изменения флагов по клиенту",
+//            dependsOnMethods = "client"
+//    )
+//    public void enableFlags() {
+//
+//        getIC().locateWorkflows()
+//                .openRecord("Клиент Workflow")
+//                .addAction()
+//                .setDisplayName("CHANGE_CLIENT_CONTACT_TRUE")
+//                .addFromState(WorkflowAction.WorkflowActionState.ANY_STATE)
+//                .addToState(WorkflowAction.WorkflowActionState.KEEP_CURRENT_STATE)
+//                .addFieldMapping("Смена учётных данных", "true", null)
+//                .save()
+//                .addAction()
+//                .setDisplayName("CHANGE_CLIENT_CONTACT_FALSE")
+//                .addFromState(WorkflowAction.WorkflowActionState.ANY_STATE)
+//                .addToState(WorkflowAction.WorkflowActionState.KEEP_CURRENT_STATE)
+//                .addFieldMapping("Смена учётных данных", "false", null)
+//                .save()
+//                .addAction()
+//                .setDisplayName("CHANGE_AUTH_IMSI_FALSE")
+//                .addFromState(WorkflowAction.WorkflowActionState.ANY_STATE)
+//                .addToState(WorkflowAction.WorkflowActionState.KEEP_CURRENT_STATE)
+//                .addFieldMapping("Изменен IMSI телефона для аутентификации", "false", null)
+//                .save()
+//                .addAction()
+//                .setDisplayName("CHANGE_AUTH_IMSI_TRUE")
+//                .addFromState(WorkflowAction.WorkflowActionState.ANY_STATE)
+//                .addToState(WorkflowAction.WorkflowActionState.KEEP_CURRENT_STATE)
+//                .addFieldMapping("Изменен IMSI телефона для аутентификации", "true", null)
+//                .save();
+//    }
+
+    @Test(
+            description = "1. Провести транзакцию № 1 \"Заявка на выпуск карты\" от имени клиента № 1, " +
+                    "у которого не взведены флаги \"Смена учетных данных\" и \"Изменен IMSI телефона для аутентификации\"",
+            dependsOnMethods = "client"
+    )
+
+    public void transaction1() {
+
+//        TODO КЛИЕНТ без флагов
+        Transaction transaction = getTransaction();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(0));
+        transactionData
+                .withInitialSourceAmount(BigDecimal.valueOf(10000))
+                .getRequestCardIssue()
+                .withAmountInSourceCurrency(BigDecimal.valueOf(500));
+        sendAndAssert(transaction);
+        assertLastTransactionRuleApply(NOT_TRIGGERED, RESULT_RULE_NOT_APPLY);
+    }
+
+    @Test(
+            description = "Провести транзакцию № 2 \"Заявка на выпуск карты\" " +
+                    "от имени клиента № 2, у которого  взведен только флаг \"Смена учетных данных\"",
+            dependsOnMethods = "transaction1"
+    )
+    public void transaction2() {
+//        TODO КЛИЕНТ с CHANGE_CLIENT_CONTACT
+
+        getIC().locateReports()
+                .openFolder("Бизнес-сущности")
+                .openRecord("Список клиентов")
+                .setTableFilterWithActive("Идентификатор клиента", "Equals", clientIds.get(0)).runReport()
+                .openFirst()
+                .getActionsForClient()
+                .doAction("CHANGE_CLIENT_CONTACT_TRUE")
+                .approved();
+        assertTableField("Изменен IMSI телефона для аутентификации:", "No");
+        assertTableField("Смена учётных данных:", "Yes");
+
+        time.add(Calendar.MINUTE, 1);
+        Transaction transaction = getTransaction();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(0));
+        transactionData
+                .withInitialSourceAmount(BigDecimal.valueOf(10000))
+                .getRequestCardIssue()
+                .withAmountInSourceCurrency(BigDecimal.valueOf(500));
+        sendAndAssert(transaction);
+        assertLastTransactionRuleApply(TRIGGERED, "Изменен контакт клиента для аутентификации");
+    }
+
+    @Test(
+            description = "Провести транзакцию № 3 \"Заявка на выпуск карты\" от имени клиента № 3, " +
+                    "у которого  взведен только флаг \"Изменен IMSI телефона для аутентификации\"",
+            dependsOnMethods = "transaction2"
+    )
+    public void transaction3() {
+//        TODO КЛИЕНТ с CHANGE_AUTH_IMSI
+
+        getIC().locateReports()
+                .openFolder("Бизнес-сущности")
+                .openRecord("Список клиентов")
+                .setTableFilterWithActive("Идентификатор клиента", "Equals", clientIds.get(0)).runReport()
+                .openFirst()
+                .getActionsForClient()
+                .doAction("CHANGE_CLIENT_CONTACT_FALSE")
+                .approved();
+        getIC().locateReports()
+                .openFolder("Бизнес-сущности")
+                .openRecord("Список клиентов")
+                .setTableFilterWithActive("Идентификатор клиента", "Equals", clientIds.get(0)).runReport()
+                .openFirst()
+                .getActionsForClient()
+                .doAction("CHANGE_AUTH_IMSI_TRUE")
+                .approved();
+        assertTableField("Изменен IMSI телефона для аутентификации:", "Yes");
+        assertTableField("Смена учётных данных:", "No");
+
+        time.add(Calendar.MINUTE, 1);
+        Transaction transaction = getTransaction();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(0));
+        transactionData
+                .withInitialSourceAmount(BigDecimal.valueOf(10000))
+                .getRequestCardIssue()
+                .withAmountInSourceCurrency(BigDecimal.valueOf(500));
+        sendAndAssert(transaction);
+        assertLastTransactionRuleApply(TRIGGERED, "Изменен IMSI телефона для аутентификации");
+    }
+
+    @Override
+    protected String getRuleName() {
+        return RULE_NAME;
+    }
+
+    private Transaction getTransaction() {
+        Transaction transaction = getTransaction("testCases/Templates/REQUEST_CARD_ISSUE.xml");
+        transaction.getData().getTransactionData()
+                .withDocumentSaveTimestamp(new XMLGregorianCalendarImpl(time))
+                .withDocumentConfirmationTimestamp(new XMLGregorianCalendarImpl(time));
+        return transaction;
+    }
+}
---
+++ src/test/java/ru/iitdgroup/tests/cases/BIQ_7739/ExR_03_UseNewDevice.java
@@ -0,0 +1,279 @@
+package ru.iitdgroup.tests.cases.BIQ_7739;
+
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+import net.bytebuddy.utility.RandomString;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+import ru.iitdgroup.intellinx.dbo.transaction.TransactionDataType;
+import ru.iitdgroup.tests.apidriver.Client;
+import ru.iitdgroup.tests.apidriver.Transaction;
+import ru.iitdgroup.tests.cases.RSHBCaseTest;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExR_03_UseNewDevice extends RSHBCaseTest {
+    private static final String RULE_NAME = "R01_ExR_03_UseNewDevice";
+    private static final String TABLE = "(System_parameters) Интеграционные параметры";
+    private final GregorianCalendar time = new GregorianCalendar();
+    private final List<String> clientIds = new ArrayList<>();
+    private String[][] names = {{"Ольга", "Петушкова", "Ильинична"}, {"Кира", "Брызгина", "Ивановна"}};
+
+    private static String LOGIN = new RandomString(5).nextString();
+    private static String LOGIN1 = new RandomString(5).nextString();
+    private static String LOGIN_HASH = (ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE) + "").substring(0, 5);
+    private static String LOGIN_HASH1 = (ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE) + "").substring(0, 5);
+    private static final String SESSION_ID = new RandomString(10).nextString();
+    private static final String SESSION_ID1 = new RandomString(10).nextString();
+
+    //TODO Тест кейс подразумевает уже наполненные справочники ГИС и включенную интеграцию с ГИС
+
+    @Test(
+            description = "Создаем клиента"
+    )
+    public void addClient() {
+        try {
+            for (int i = 0; i < 2; i++) {
+                String[] dboId = {(ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE) + "").substring(0, 9),
+                        (ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE) + "").substring(0, 9)};
+                Client client = new Client("testCases/Templates/client.xml");
+                if (i == 0) {
+                    client.getData().getClientData().getClient().withLogin(LOGIN);
+                } else {
+                    client.getData().getClientData().getClient().withLogin(LOGIN1);
+                }
+
+                if (i == 0) {
+                    client.getData().getClientData().getClient().getClientIds().withLoginHash(LOGIN_HASH);
+                } else {
+                    client.getData().getClientData().getClient().getClientIds().withLoginHash(LOGIN_HASH1);
+                }
+
+                client.getData()
+                        .getClientData()
+                        .getClient()
+                        .withPasswordRecoveryDateTime(time)
+                        .withFirstName(names[i][0])
+                        .withLastName(names[i][1])
+                        .withMiddleName(names[i][2])
+                        .getClientIds()
+                        .withDboId(dboId[i])
+                        .withCifId(dboId[i])
+                        .withExpertSystemId(dboId[i])
+                        .withEksId(dboId[i])
+                        .getAlfaIds()
+                        .withAlfaId(dboId[i]);
+
+                sendAndAssert(client);
+                clientIds.add(dboId[i]);
+                System.out.println(dboId[i]);
+            }
+        } catch (JAXBException | IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Test(
+            description = "Настройка и включение правила",
+            dependsOnMethods = "addClient"
+    )
+    public void enableRules() {
+        getIC().locateRules()
+                .selectVisible()
+                .deactivate()
+                .editRule(RULE_NAME)
+                .fillCheckBox("Active:", true)
+                .save()
+                .detachWithoutRecording("Коды ответов ВЭС")
+                .attachVESCode46("Коды ответов ВЭС")
+                .sleep(10);
+
+        getIC().locateTable(TABLE)
+                .findRowsBy()
+                .match("Код значения", "IntegrVES2")
+                .click()
+                .edit()
+                .fillInputText("Значение:", "0")
+                .save();
+    }
+
+    @Test(
+            description = "Провести транзакцию № 1 \"Заявка на выпуск карты\" в интернет-банке с выключенной интеграцией" +
+                    "Включить интеграцию с ВЭС и  Установить VES_TIMEOUT в 0 мс",
+            dependsOnMethods = "enableRules"
+    )
+    public void transaction1() {
+        Transaction transaction = getTransactionPC();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(0));
+        sendAndAssert(transaction);
+        assertLastTransactionRuleApply(NOT_TRIGGERED, DISABLED_INTEGR_VES_NEW);
+
+        getIC().locateTable("(System_parameters) Интеграционные параметры")
+                .findRowsBy()
+                .match("Код значения", "IntegrVES2")
+                .click()
+                .edit()
+                .fillInputText("Значение:", "1")
+                .save();
+
+        getIC().locateTable("(System_parameters) Интеграционные параметры")
+                .findRowsBy()
+                .match("Код значения", "VES_TIMEOUT")
+                .click()
+                .edit()
+                .fillInputText("Значение:", "0")
+                .save();
+    }
+
+    @Test(
+            description = "Провести транзакцию \"Заявка на выпуск карты\" № 2 в интернет-банке" +
+                    "и после Установить VES_TIMEOUT в 10000 мс",
+            dependsOnMethods = "transaction1"
+    )
+    public void transaction2() {
+        Transaction transaction = getTransactionPC();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(0));
+        sendAndAssert(transaction);
+        assertLastTransactionRuleApply(FEW_DATA, RESULT_FEW_DATA_NEW);
+
+        getIC().locateTable("(System_parameters) Интеграционные параметры")
+                .findRowsBy()
+                .match("Код значения", "VES_TIMEOUT")
+                .click()
+                .edit()
+                .fillInputText("Значение:", "300")
+                .save();
+    }
+
+    @Test(
+            description = "Провести транзакцию № 3 \"Заявка на выпуск карты\", в интернет-банке",
+            dependsOnMethods = "transaction2"
+    )
+    public void transaction3() {
+        try {
+            String vesResponse = getRabbit().getVesResponse();
+            JSONObject json = new JSONObject(vesResponse);
+            json.put("customer_id", clientIds.get(0));
+            json.put("type_id", "46");
+            json.put("login", LOGIN);
+            json.put("login_hash", LOGIN_HASH);
+            json.put("time", "2021-02-02T08:20:35+03:00");
+            json.put("session_id", SESSION_ID);
+            json.put("device_hash", SESSION_ID);
+            String newStr = json.toString();
+            getRabbit().setVesResponse(newStr);
+            getRabbit().sendMessage();
+
+        } catch (JSONException e) {
+            throw new IllegalStateException();
+        }
+
+        Transaction transaction = getTransaction();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(0));
+        transactionData
+                .withSessionId(SESSION_ID);
+        sendAndAssert(transaction);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertLastTransactionRuleApply(TRIGGERED, RESULT_ALERTS);
+    }
+
+
+    @Test(
+            description = "Провести транзакцию № 4 \"Заявка на выпуск карты\", в мобильном-банке",
+            dependsOnMethods = "transaction3"
+    )
+    public void transaction4() {
+        Transaction transaction = getTransactionPC();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(0));
+        transactionData
+                .withSessionId(SESSION_ID);
+        sendAndAssert(transaction);
+        assertLastTransactionRuleApply(TRIGGERED, RESULT_ALERTS);
+    }
+
+    @Test(
+            description = "Провести транзакцию № 5 не содержащую в ответе код 46 (например, несуществующий sessionid)",
+            dependsOnMethods = "transaction4"
+    )
+    public void transaction5() {
+        Transaction transaction = getTransaction();
+        TransactionDataType transactionData = transaction.getData().getTransactionData()
+                .withRegular(false);
+        transactionData
+                .getClientIds()
+                .withDboId(clientIds.get(1));
+        transactionData
+                .withSessionId(SESSION_ID1);
+        try {
+            JSONObject js = new JSONObject(getRabbit().getVesResponse());
+            js.put("customer_id", clientIds.get(1));
+            js.put("type_id", "22");
+            js.put("login", LOGIN1);
+            js.put("login_hash", LOGIN_HASH1);
+            js.put("time", "2021-02-02T08:20:35+03:00");
+            js.put("session_id", SESSION_ID1);
+            js.put("device_hash", SESSION_ID1);
+            String vesMessage = js.toString();
+            getRabbit().setVesResponse(vesMessage);
+            getRabbit().sendMessage();
+            getRabbit().close();
+        } catch (JSONException e) {
+            throw new IllegalStateException();
+        }
+        sendAndAssert(transaction);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertLastTransactionRuleApply(NOT_TRIGGERED, RESULT_RULE_NOT_APPLY);
+    }
+
+    @Override
+    protected String getRuleName() {
+        return RULE_NAME;
+    }
+
+    private Transaction getTransactionPC() {
+        Transaction transaction = getTransaction("testCases/Templates/REQUEST_CARD_ISSUE_PC.xml");
+        transaction.getData().getTransactionData()
+                .withDocumentSaveTimestamp(new XMLGregorianCalendarImpl(time))
+                .withDocumentConfirmationTimestamp(new XMLGregorianCalendarImpl(time));
+        return transaction;
+    }
+
+    private Transaction getTransaction() {
+        Transaction transaction = getTransaction("testCases/Templates/REQUEST_CARD_ISSUE.xml");
+        transaction.getData().getTransactionData()
+                .withDocumentSaveTimestamp(new XMLGregorianCalendarImpl(time))
+                .withDocumentConfirmationTimestamp(new XMLGregorianCalendarImpl(time));
+        return transaction;
+    }
+}
